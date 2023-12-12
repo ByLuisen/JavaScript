1be6b78c7b3237b4774eb14dbb36be5e2c1b693b
Luis_Enrique_Ledesma_Ollague <luisenric32@gmail.com>
2023-12-13 00:54:08 +0100

    Cambiar versiones de Angular y Node

---
+++ UF3/app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//cargados automaticamente
+import { Componente1Component } from './componentes/componente1/componente1.component';
+import { Componente2Component } from './componentes/componente2/componente2.component';
+
+
+//cargar a mano
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Componente1Component,
+    Componente2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
